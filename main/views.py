801a4bf9abe0713daf28b05e51495baee571aa6b
SkylarYHU <skylarhyn@gmail.com>
2024-10-17 23:54:19 +0100

    Update views

--- main/views.py
+++ main/views.py
@@ -1,25 +1,28 @@
-from django.shortcuts import render
-
-# Create your views here.
+import os
 import json
 import urllib.request
 from urllib.parse import quote
-import os
+from django.shortcuts import render
 
 
 def index(request):
     if request.method == "POST":
         city = request.POST['city']
-
         formatted_city = quote(city)
-        api_key = os.getenv('API_KEY')
+
+        # 从环境变量获取 API 密钥
+        api_key = os.environ.get("OPENWEATHER_API_KEY")
+
+        # 确保 API 密钥存在
+        if not api_key:
+            return render(request, "main/index.html", {"error": "API key is not set."})
+
         url = f'https://api.openweathermap.org/data/2.5/weather?units=metric&q={
             formatted_city}&appid={api_key}'
 
         try:
             response = urllib.request.urlopen(url)
             source = response.read().decode('utf-8')
-
             list_of_data = json.loads(source)
 
             data = {
@@ -29,7 +32,6 @@ def index(request):
                 "pressure": str(list_of_data['main']['pressure']) + ' hPa',
                 "humidity": str(list_of_data['main']['humidity']) + '%',
             }
-
         except Exception as e:
             data = {"error": str(e)}
 
